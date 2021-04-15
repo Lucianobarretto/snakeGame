36b3c747584d79c833846e964202d1809342dfad
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-04-15 14:10:19 -0300

    Estruturando x,y

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let box = 32;
 let snake = [];
 
 snake[0] = {
-  x: 8 * box,
-  y: 8 * box
+  x: 7 * box,
+  y: 7 * box
 }
+
+let direction = "right";
 
 function createBG() {
   context.fillStyle = "black";
@@ -20,5 +22,26 @@ function createSnake() {
   }
 }
 
-createBG();
-createSnake();
+function startGame() {
+  createBG();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == 'right') snakeX += box;
+  if(direction == 'left') snakeY -= box;
+  if(direction =='up') snakeY -= box;
+  if(direction == 'down')snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
